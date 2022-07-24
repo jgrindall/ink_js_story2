fbc0eaef31fd2d18a6c444e9b162642cdea7b8fd
jgrindall <john.grindall@gmail.com>
2022-07-24 19:14:42 +0100

    tests

--- src/ink/Utils.ts
+++ src/ink/Utils.ts
@@ -1,4 +1,11 @@
-import { receiveMessageOnPort } from "worker_threads";
+interface Rect{
+    top:number,
+    left:number,
+    right:number,
+    width:number,
+    height:number,
+    bottom:number
+}
 
 const merge = <T>(arr:T[]) : T => {
     return arr.reduce((memo: T, current: T)=>{
@@ -9,7 +16,7 @@ const merge = <T>(arr:T[]) : T => {
     }, {} as T)
 };
 
-const isContained = (rect:DOMRect | undefined, containerRect: DOMRect | undefined): boolean => {
+const isContained = (rect:Rect | undefined, containerRect: Rect | undefined): boolean => {
     return !!(containerRect && rect && rect.top >= containerRect.top && rect.bottom <= containerRect.bottom);
 };
 
@@ -20,12 +27,33 @@ const isContainedIn = (container: HTMLElement)=>{
     };
 };
 
-const getOverlapPercent = (rect:DOMRect, containerRect: DOMRect): number => {
+const getOverlapPercent = (rect:Rect, containerRect: Rect, options :{x:boolean, y:boolean} = {x:false, y:true}): number => {
     const overlap = getOverlap(rect, containerRect);
-    return overlap ? 100 * overlap?.height / rect?.height : 0;
+    let numerator:number;
+    let denominator:number;
+    if(overlap){
+        if(options.x && options.y){
+            numerator = overlap.height * overlap.width;
+            denominator = rect.width * rect.height;
+        }
+        else if(options.x && !options.y){
+            numerator = overlap.width;
+            denominator = rect.width;
+        }
+        else {
+            numerator = overlap.height;
+            denominator = rect.height;
+        }
+        return 100*numerator/denominator;
+    }
+    return 0;
 }
 
-const getOverlap = (rectA:DOMRect, rectB: DOMRect): {width:number, height:number} | undefined => {
+const getOverlapPercentEl = (el:HTMLElement, container: HTMLElement):number =>{
+    return getOverlapPercent(el.getBoundingClientRect(), container.getBoundingClientRect());
+};
+
+const getOverlap = (rectA:Rect, rectB: Rect): {width:number, height:number} | undefined => {
     const x0 = Math.max(rectA.left, rectB.left);
     const x1 = Math.min(rectA.right, rectB.right);
     const y0 = Math.max(rectA.top, rectB.top);
@@ -54,6 +82,7 @@ export {
     isContained,
     isContainedIn,
     getOverlapPercent,
+    getOverlapPercentEl,
     getOverlap,
     merge,
     id,
--- src/ink/__tests__/Utils.spec.ts
+++ src/ink/__tests__/Utils.spec.ts
@@ -1,10 +1,59 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import {getOverlap} from '../Utils'
+import {getOverlap, getOverlapPercent} from '../Utils'
 
-describe('1', () => {
-  it('2', () => {
-    expect(getOverlap).toBeTruthy();
-  })
+describe('test Utils', () => {
+    
+    it('check truthy', () => {
+      expect(getOverlap).toBeTruthy();
+    })
+
+    it('check basic overlap', () => {
+      const r1 = {left:0, right:100, width:100, height:100, top:0, bottom:100};
+      const r2 = {left:100, right:200, width:100, height:100, top:0, bottom:100};
+      expect(getOverlapPercent(r1, r2)).toEqual(0);
+      expect(getOverlapPercent(r1, r1)).toEqual(100);
+      expect(getOverlapPercent(r2, r2)).toEqual(100);
+    })
+
+    it('check three squares', () => {
+      const r1 = {left:0, right:100, width:100, height:100, top:0, bottom:100};
+      const r2 = {left:50, right:150, width:100, height:100, top:0, bottom:100};
+      const r3 = {left:0, right:100, width:100, height:100, top:50, bottom:150};
+      
+      expect(getOverlapPercent(r1, r2, {x: true, y:false})).toEqual(50);
+      expect(getOverlapPercent(r1, r2, {x: false, y:true})).toEqual(100);
+      expect(getOverlapPercent(r1, r2, {x: true, y:true})).toEqual(50);
+
+      expect(getOverlapPercent(r1, r3, {x: true, y:false})).toEqual(100);
+      expect(getOverlapPercent(r1, r3, {x: false, y:true})).toEqual(50);
+      expect(getOverlapPercent(r1, r3, {x: true, y:true})).toEqual(50);
+
+      expect(getOverlapPercent(r2, r3, {x: true, y:false})).toEqual(50);
+      expect(getOverlapPercent(r2, r3, {x: false, y:true})).toEqual(50);
+      expect(getOverlapPercent(r2, r3, {x: true, y:true})).toEqual(25);
+
+    })
+
+    it('check a typical story use-case', () => {
+      const r1 = {left:0, right:100, width:100, height:20, top:90, bottom:110};
+      const r2 = {left:0, right:100, width:100, height:100, top:0, bottom:100};
+      
+      expect(getOverlapPercent(r1, r2, {x: true, y:false})).toEqual(100);
+      expect(getOverlapPercent(r1, r2, {x: false, y:true})).toEqual(50);
+      expect(getOverlapPercent(r1, r2, {x: true, y:true})).toEqual(50);
+
+    })
+
+    it('check another typical story use-case', () => {
+      const r1 = {left:50, right:100, width:50, height:20, top:90, bottom:110};
+      const r2 = {left:0, right:100, width:100, height:100, top:0, bottom:100};
+      
+      expect(getOverlapPercent(r1, r2, {x: true, y:false})).toEqual(100);
+      expect(getOverlapPercent(r1, r2, {x: false, y:true})).toEqual(50);
+      expect(getOverlapPercent(r1, r2, {x: true, y:true})).toEqual(50);
+
+    })
+
 })
