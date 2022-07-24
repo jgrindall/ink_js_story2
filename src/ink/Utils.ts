01b3c28670cea945e01ec6f7a166838bb5137dc7
jgrindall <john.grindall@gmail.com>
2022-07-24 17:58:09 +0100

    start some tests

--- src/ink/Utils.ts
+++ src/ink/Utils.ts
@@ -1,3 +1,5 @@
+import { receiveMessageOnPort } from "worker_threads";
+
 const merge = <T>(arr:T[]) : T => {
     return arr.reduce((memo: T, current: T)=>{
         return {
@@ -18,8 +20,23 @@ const isContainedIn = (container: HTMLElement)=>{
     };
 };
 
-const getOverlapPercent = (rect:DOMRect | undefined, containerRect: DOMRect | undefined): number => {
-    return 50;
+const getOverlapPercent = (rect:DOMRect, containerRect: DOMRect): number => {
+    const overlap = getOverlap(rect, containerRect);
+    return overlap ? 100 * overlap?.height / rect?.height : 0;
+}
+
+const getOverlap = (rectA:DOMRect, rectB: DOMRect): {width:number, height:number} | undefined => {
+    const x0 = Math.max(rectA.left, rectB.left);
+    const x1 = Math.min(rectA.right, rectB.right);
+    const y0 = Math.max(rectA.top, rectB.top);
+    const y1 = Math.min(rectA.bottom, rectB.bottom);
+    if(x0 < x1 && y0 < y1){
+        return {
+            width:x1 - x0,
+            height:y1 - y0
+        };
+    }
+    return undefined;
 }
 
 let _id = 0;
@@ -37,6 +54,7 @@ export {
     isContained,
     isContainedIn,
     getOverlapPercent,
+    getOverlap,
     merge,
     id,
     count
---
+++ src/ink/__tests__/Utils.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import {getOverlap} from '../Utils'
+
+describe('1', () => {
+  it('2', () => {
+    expect(getOverlap).toBeTruthy();
+  })
+})
